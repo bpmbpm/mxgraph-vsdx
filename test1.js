6c4889a43b17759040ecd89e33cef295efecc6b8
bpmbpm <121898072+bpmbpm@users.noreply.github.com>
2024-08-01 13:41:14 +0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,5 +1,5 @@
 // JavaScript Document
 //   alert( 'xxx' );
 // import doImportVisio from "mxgraph-vsdx";
-import doImportVisio from "./index.js";
+import doImportVisio from "./index.js"; // SyntaxError: The requested module './index.js' does not provide an export named 'default'
 console.log('Hello doImportVisio');

1e4b07c8ff14a00c38d96fdff32e15120e7c18c7
bpmbpm <121898072+bpmbpm@users.noreply.github.com>
2024-08-01 13:39:07 +0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,4 +1,5 @@
 // JavaScript Document
 //   alert( 'xxx' );
-import doImportVisio from "mxgraph-vsdx";
+// import doImportVisio from "mxgraph-vsdx";
+import doImportVisio from "./index.js";
 console.log('Hello doImportVisio');
